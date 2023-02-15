66eca637c388ba82d03d3b24474a308d9b451b1f
godalming123 <jamiesamglasson@gmail.com>
2023-02-15 10:43:10 +0000

    fix my workspaces with british keyboards

--- dwl/src/config.h
+++ dwl/src/config.h
@@ -120,8 +120,8 @@ static const Key keys[] = {
 	{ MODKEY,                    XKB_KEY_f,                     togglefullscreen, {0} },
 	{ MODKEY,                    XKB_KEY_Escape,                quit,             {0} },
 	TAGKEYS(                     XKB_KEY_1, XKB_KEY_exclam,                       0),
-	TAGKEYS(                     XKB_KEY_2, XKB_KEY_at,                           1),
-	TAGKEYS(                     XKB_KEY_3, XKB_KEY_numbersign,                   2),
+	TAGKEYS(                     XKB_KEY_2, XKB_KEY_quotedbl,                     1),
+	TAGKEYS(                     XKB_KEY_3, XKB_KEY_sterling,                     2),
 	TAGKEYS(                     XKB_KEY_4, XKB_KEY_dollar,                       3),
 	TAGKEYS(                     XKB_KEY_5, XKB_KEY_percent,                      4),
 
